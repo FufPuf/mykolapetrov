abaf78f7090061f6bd06411dc5a3e834e94e8406
mykola.petrov mykola.petrov@vaimo.com <mykola.petrov@vaimo.com>
2021-02-18 17:29:23 +0200

    add js + style

---
+++ app/design/frontend/Nikolay/nikolay-theme/Nikolay_Brand/requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+        map: {
+            '*': {
+                slick: 'Nikolay_Brand/js/slick.min',
+                carousel: 'Nikolay_Brand/js/brands-carousel'
+            }
+
+        },
+        shim: {
+            slick: {
+                deps: ['jquery']
+            }
+        }
+    }
+;
